78a0aa997c9d18c62806ee562e387d94d97fa0ab
mewben <melvinsoldia@gmail.com>
2016-08-16 17:23:22 +0800

    After eject

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux'
 import './index.css';
 
+import App from './App';
+import configureStore from 'store/configureStore'
+
+const store = configureStore()
+
+
 ReactDOM.render(
-  <App />,
+	<Provider store={store}>
+		<App />
+	</Provider>,
   document.getElementById('root')
 );
